1ab91d3a2cdac42afcae7e2ea3817bf662e7b720
Erik Oestlyngen <erik.oestlyngen@gmx.de>
2022-05-24 13:44:39 +0200

    added links to social media

--- scripts.js
+++ scripts.js
@@ -8,3 +8,23 @@ function hideMenu() {
   let navLinks = document.getElementById("nav-links");
   navLinks.style.right = "-200px";
 }
+//redirecting to various social media sites
+function redirect(element) {
+  switch (element.className) {
+    case "fa-brands fa-facebook":
+      window.location.href = "https://www.facebook.com/";
+      break;
+    case "fa-brands fa-twitter":
+      window.location.href = "https://twitter.com/";
+      break;
+    case "fa-brands fa-instagram":
+      window.location.href = "https://www.instagram.com/";
+      break;
+    case "fa-brands fa-linkedin":
+      window.location.href = "https://www.linkedin.com/";
+      break;
+    case "fa-brands fa-github":
+      window.location.href = "https://www.github.com/shourtugal";
+      break;
+  }
+}
